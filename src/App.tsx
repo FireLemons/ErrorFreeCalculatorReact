a1cc982ee199c866cebb27bdbfa77ce2dc18238c
firelemons <shenyang64@gmail.com>
2023-01-05 20:06:12 -0600

    add calculator html

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> </p>
+      <div className="buttons">
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+        </div>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
+        <div>
+          <button>-</button>
+          <button>0</button>
+          <button>+</button>
+        </div>
+        <div>
+          <button> = </button>
+        </div>
+      </div>
     </div>
   );
 }
